13b5de3fb6769a2457ef430aaf46721b84fa613d
Ansh Nimbalkar <z5415892@ad.unsw.edu.au>
2023-10-03 00:31:36 +1100

    Implementation for adminAuthRegister function

--- src/auth.js
+++ src/auth.js
@@ -1,6 +1,37 @@
+import {getData, setData} from './dataStore.js';
+
+import validator from 'validator';
+
 function adminAuthRegister( email, password, nameFirst, nameLast ) {
+    let data = getData();
+    if (validator.isEmail(email) === false) {
+        return {error: 'Invalid Entry'};
+    }
+    for (let i in data.users) {
+        if (data.users[i].email === email) {
+            return {error: 'Invalid Entry'};
+        }
+    }
+    const pattern = /^[a-zA-Z\s\-']+$/;
+    if (nameFirst.length < 2 || nameFirst.length > 20 || pattern.test(nameFirst) === false) {
+        return {error: 'Invalid Entry'};
+    }
+    if (nameLast.length < 2 || nameLast.length > 20 || pattern.test(nameLast) === false) {
+        return {error: 'Invalid Entry'};
+    }
+    const letters = /[a-zA-Z]/;
+    const nums = /\d/;
+    if (password.length < 8 || letters.test(password) === false || nums.test(password) === false) {
+        return {error: 'Invalid Entry'};
+    }
+
+    const arrayindex = data.users.length;
+    const Id = data.users[arrayindex - 1].userId + 10;
+
+    data.users.push({userId: Id, email: email, password: password, nameFirst: nameFirst, nameLast: nameLast, numSuccessfulLogins: 0, numFailedPasswordsSinceLastLogin: 0});
+
     return {
-        authUserId: 1
+        authUserId: data.users[arrayindex].userId
     }
 }
 
@@ -22,3 +53,9 @@ function adminUserDetails(authUserId) {
             }
     }
 }
+
+/*console.log(adminAuthRegister('validemail@gmail.com', 'password123', 'pedri', 'gonzalez'));
+console.log(adminAuthRegister('validemail1@gmail.com', 'PASSword1', 'pedri', 'gonzalez'));
+console.log(getData());*/
+
+export {adminAuthRegister} 
