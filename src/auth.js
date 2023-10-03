bb116ac0df08a8268d0b588d0d35405f1bd5a9ac
Jonathan Leung <z5479360@ad.unsw.edu.au>
2023-10-03 19:07:56 +1100

    completed functionality for adminUserDetails

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,7 @@
+import {getData, setData} from './dataStore.js';
+
+import validator from 'validator';
+
 function adminAuthRegister( email, password, nameFirst, nameLast ) {
     return {
         authUserId: 1
@@ -11,14 +15,22 @@ function adminAuthLogin( email, password ) {
 }
 
 function adminUserDetails(authUserId) {
-    return {
-        user:
-            {
-              userId: 1,
-              name: 'Hayden Smith',
-              email: 'hayden.smith@unsw.edu.au',
-              numSuccessfulLogins: 3,
-              numFailedPasswordsSinceLastLogin: 1,
-            }
+    let data = getData();
+    const user = data.users.find((user) => user.userId === authUserId);
+    if (!user) {
+        return { error: 'Invalid User'};
     }
-}
+    return {
+        user 
+    };
+     // user:
+        //     {
+        //       userId: 1,
+        //       name: 'Hayden Smith',
+        //       email: 'hayden.smith@unsw.edu.au',
+        //       numSuccessfulLogins: 3,
+        //       numFailedPasswordsSinceLastLogin: 1,
+        //     }
+};
+
+export {adminUserDetails}
