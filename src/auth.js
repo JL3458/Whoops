47e3eaf85e95969ab3a6fd9bb3beb82ec25b954e
Ansh Nimbalkar <z5415892@ad.unsw.edu.au>
2023-10-03 19:34:22 +1100

    Implementation for authregister

--- src/auth.js
+++ src/auth.js
@@ -9,26 +9,27 @@ function adminAuthRegister( email, password, nameFirst, nameLast ) {
     }
     for (let i in data.users) {
         if (data.users[i].email === email) {
-            return {error: 'Invalid Entry'};
+            return {error: 'Email not found'};
         }
     }
     const pattern = /^[a-zA-Z\s\-']+$/;
     if (nameFirst.length < 2 || nameFirst.length > 20 || pattern.test(nameFirst) === false) {
-        return {error: 'Invalid Entry'};
+        return {error: 'Invalid First Name'};
     }
     if (nameLast.length < 2 || nameLast.length > 20 || pattern.test(nameLast) === false) {
-        return {error: 'Invalid Entry'};
+        return {error: 'Invalid Last Name'};
     }
     const letters = /[a-zA-Z]/;
     const nums = /\d/;
     if (password.length < 8 || letters.test(password) === false || nums.test(password) === false) {
-        return {error: 'Invalid Entry'};
+        return {error: 'Invalid Password'};
     }
 
     const arrayindex = data.users.length;
     const Id = data.users[arrayindex - 1].userId + 10;
 
     data.users.push({userId: Id, email: email, password: password, nameFirst: nameFirst, nameLast: nameLast, numSuccessfulLogins: 0, numFailedPasswordsSinceLastLogin: 0});
+    setData(data);
 
     return {
         authUserId: data.users[arrayindex].userId
