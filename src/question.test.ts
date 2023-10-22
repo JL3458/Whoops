0c8fd5aa159757ba042877cadd1da0376af8d847
Divakar Dessai <z5411688@ad.unsw.edu.au>
2023-10-22 20:53:23 +1100

    Added Server Tests for adminQuizCreateQuestions in question.test.ts and Implemented adminQuizCreateQuestion in question.ts, added question interfaces in dataStore.js and server request for the same function in server.ts

---
+++ src/question.test.ts
@@ -0,0 +1,551 @@
+import request from 'sync-request-curl';
+import { port, url } from './config.json';
+import { clearRequest } from './other.test';
+import { authRegisterRequest } from './auth.test';
+import { adminQuizCreateRequest } from './quiz.test'; 
+import { questionBody } from './question'; 
+
+const SERVER_URL = `${url}:${port}`;
+const ERROR = { error: expect.any(String) };
+const QUESTIONID = { questionId: expect.any(Number)};
+
+/////////////////////// Helper Functions //////////////////////
+
+export function adminQuizCreateQuestionRequest(token: string, quizid: number, question: questionBody) {
+    const request1 = request('POST', SERVER_URL + `/v1/admin/quiz/${quizid}/question`, { json: { token: token, question: question} });
+    return JSON.parse(request1.body as string);
+}
+
+////////////////////////// Main Tests /////////////////////////////
+// TODO: Add relevant tests calling the server.ts files
+
+describe('Tests of adminQuizCreateQuestion', () => {
+    beforeEach(() => {
+      clearRequest();
+    });
+  
+    test('Token is Empty or Invalid', () => {
+        const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+        const newQuiz = adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'This is a test');
+        const newQuestion = 
+        {
+            questionTitle: 'Sample Question 1',
+            duration: 5,
+            points: 4,
+            answers: [
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Charles',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest('', newQuiz.quizId, newQuestion)).toEqual(ERROR);
+    });
+
+    test('Valid token is provided, but user is not an owner of this quiz', () => {
+        const newUser1 = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+        const newUser2 = authRegisterRequest('Validemail2@gmail.com', 'password123', 'Pattrick', 'Dessai');
+        const newQuiz = adminQuizCreateRequest(newUser1.token, 'Test Quiz 1', 'This is a test');
+        const newQuestion = 
+        {
+            questionTitle: 'Sample Question 1',
+            duration: 5,
+            points: 4,
+            answers: [
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Charles',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser2.token, newQuiz.quizId, newQuestion)).toEqual(ERROR);
+    });
+
+    test('Test For Normal Case', () => {
+        const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+        const newQuiz = adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'This is a test');
+        const newQuestion = 
+        {
+            questionTitle: 'Sample Question 1',
+            duration: 5,
+            points: 4,
+            answers: [
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Charles',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser.token, newQuiz.quizId, newQuestion)).toEqual(QUESTIONID);
+    });
+
+    test('Test For Invalid QuizId', () => {
+        const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+        const newQuiz = adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'This is a test');
+        const newQuestion = 
+        {
+            questionTitle: 'Sample Question 1',
+            duration: 5,
+            points: 4,
+            answers: [
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Charles',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser.token, newQuiz.quizId + 1, newQuestion)).toEqual(ERROR);
+    });
+
+    test('Test For Invalid Question String', () => {
+        const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+        const newQuiz = adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'This is a test');
+        let newQuestion = 
+        {
+            questionTitle: 'Four',
+            duration: 5,
+            points: 4,
+            answers: [
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Charles',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser.token, newQuiz.quizId, newQuestion)).toEqual(ERROR);
+        newQuestion = 
+        {
+            questionTitle: 'Exploring new frontiers broadens our horizons and enriches our lives',
+            duration: 5,
+            points: 4,
+            answers: [
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Charles',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser.token, newQuiz.quizId, newQuestion)).toEqual(ERROR);
+    });
+
+    test('Test For Number of Answers in Question', () => {
+        const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+        const newQuiz = adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'This is a test');
+        let newQuestion = 
+        {
+            questionTitle: 'Sample Question 1',
+            duration: 5,
+            points: 4,
+            answers: [
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser.token, newQuiz.quizId, newQuestion)).toEqual(ERROR);
+        newQuestion = 
+        {
+            questionTitle: 'Sample Question 1',
+            duration: 5,
+            points: 4,
+            answers: [
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Charles',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Raina',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Gill',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Kohli',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser.token, newQuiz.quizId, newQuestion)).toEqual(ERROR);
+    });
+
+    test('Test For Invalid QuizId', () => {
+        const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+        const newQuiz = adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'This is a test');
+        const newQuestion = 
+        {
+            questionTitle: 'Sample Question 1',
+            duration: 5,
+            points: 4,
+            answers: [
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Charles',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser.token, newQuiz.quizId + 1, newQuestion)).toEqual(ERROR);
+    });
+
+    test('Test For Question Duration Positive', () => {
+        const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+        const newQuiz = adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'This is a test');
+        let newQuestion = 
+        {
+            questionTitle: 'Sample Question 1',
+            duration: -1,
+            points: 4,
+            answers: [
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Charles',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser.token, newQuiz.quizId, newQuestion)).toEqual(ERROR);
+        newQuestion = 
+        {
+            questionTitle: 'Sample Question 1',
+            duration: 0,
+            points: 4,
+            answers: [
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Charles',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser.token, newQuiz.quizId, newQuestion)).toEqual(QUESTIONID);
+    });
+
+    test('Test For Sum of Duration Exceeding 180 seconds/3 minutes', () => {
+        const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+        const newQuiz = adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'This is a test');
+        let newQuestion = 
+        {
+            questionTitle: 'Sample Question 1',
+            duration: 90,
+            points: 4,
+            answers: [
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Charles',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser.token, newQuiz.quizId, newQuestion)).toEqual(QUESTIONID);
+        newQuestion = 
+        {
+            questionTitle: 'Sample Question 2',
+            duration: 100,
+            points: 4,
+            answers: [
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Charles',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser.token, newQuiz.quizId, newQuestion)).toEqual(ERROR);
+    });
+
+    test('Test For Invalid Points', () => {
+        const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+        const newQuiz = adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'This is a test');
+        let newQuestion = 
+        {
+            questionTitle: 'Sample Question 1',
+            duration: 5,
+            points: 0,
+            answers: [
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Charles',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser.token, newQuiz.quizId, newQuestion)).toEqual(ERROR);
+        newQuestion = 
+        {
+            questionTitle: 'Sample Question 1',
+            duration: 5,
+            points: 11,
+            answers: [
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Charles',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser.token, newQuiz.quizId, newQuestion)).toEqual(ERROR);
+    });
+
+    test('Test For Invalid Answer Length', () => {
+        const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+        const newQuiz = adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'This is a test');
+        let newQuestion = 
+        {
+            questionTitle: 'Sample Question 1',
+            duration: 5,
+            points: 4,
+            answers: [
+                {
+                    answerTitle: '',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Charles',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser.token, newQuiz.quizId, newQuestion)).toEqual(ERROR);
+        newQuestion = 
+        {
+            questionTitle: 'Sample Question 1',
+            duration: 5,
+            points: 4,
+            answers: [
+                {
+                    answerTitle: 'Exploring new frontiers broadens our horizons and enriches our lives',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Charles',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser.token, newQuiz.quizId, newQuestion)).toEqual(ERROR);
+    });
+
+    test('Test For Duplicate Strings', () => {
+        const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+        const newQuiz = adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'This is a test');
+        let newQuestion = 
+        {
+            questionTitle: 'Sample Question 1',
+            duration: 5,
+            points: 4,
+            answers: [
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser.token, newQuiz.quizId, newQuestion)).toEqual(ERROR);
+        newQuestion = 
+        {
+            questionTitle: 'Sample Question 1',
+            duration: 5,
+            points: 4,
+            answers: [
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser.token, newQuiz.quizId, newQuestion)).toEqual(ERROR);
+    });
+
+    test('Test For No Correct Answers', () => {
+        const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+        const newQuiz = adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'This is a test');
+        let newQuestion = 
+        {
+            questionTitle: 'Sample Question 1',
+            duration: 5,
+            points: 4,
+            answers: [
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: false
+                },
+                {
+                    answerTitle: 'Prince Charles',
+                    correct: false
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: false
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser.token, newQuiz.quizId, newQuestion)).toEqual(ERROR);
+    });
+
+  });
+
+/////////////////////////// Epilouge //////////////////////////////
+
+/*
+
+/For ease of coding/
+
+ test('Test For Normal Case', () => {
+        const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+        const newQuiz = adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'This is a test');
+        const newQuestion = 
+        {
+            questionTitle: 'Sample Question 1',
+            duration: 5,
+            points: 4,
+            answers: [
+                {
+                    answerTitle: 'Prince Wales',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Charles',
+                    correct: true
+                },
+                {
+                    answerTitle: 'Prince Diana',
+                    correct: true
+                }
+            ]
+        }
+        expect(adminQuizCreateQuestionRequest(newUser.token, newQuiz.quizId, newQuestion)).toEqual(QUESTIONID);
+    });
+
+*/
---
+++ src/question.ts
@@ -0,0 +1,124 @@
+import { getData, setData, answer} from './dataStore';
+import { authRegisterRequest } from './auth.test';
+import { adminQuizCreateRequest } from './quiz.test'; 
+
+//////////////////// Function Return Interfaces ///////////////////
+
+export interface questionBody {
+    questionTitle: string,
+    duration: number,
+    points: number,
+    answers: answer[]
+}
+
+interface ErrorReturn {
+    error: string;
+}
+
+interface QuestionCreateReturn {
+    questionId: number,
+}
+
+/////////////////////// Helper Functions //////////////////////////
+
+
+/////////////////////// Main Functions ///////////////////////////
+
+export function adminQuizCreateQuestion (token: number, quizId: number, question: questionBody): QuestionCreateReturn | ErrorReturn {   
+    let data = getData();
+    
+    // If Token is an empty string
+    if (token === '') {
+    return { error: 'Token is empty or invalid' };
+    }
+
+    // Converts token string to token object
+    const tempToken = JSON.parse(decodeURIComponent(token));
+
+    // Checks if Token is Empty or invalid
+    if (!tempToken || data.tokens.find((currentToken) => currentToken.userId === tempToken.userId) === undefined) {
+        return { error: 'Token is empty or invalid' };
+    }
+
+    // Checks if quizId refers to an invalid quiz
+    const tempQuiz = data.quizzes.find((quiz) => quiz.quizId === quizId);
+    if (tempQuiz === undefined) {
+      return { error: 'quizId is not of a valid quiz' };
+    }
+  
+    // Checks if the quiz belongs to the current logged in user
+    if (tempQuiz !== undefined && tempQuiz.userId !== tempToken.userId) {
+      return { error: 'Valid token is provided, but user is not an owner of this quiz' };
+    }
+
+    // Checks if Question string is valid
+    if (question.questionTitle.length < 5 || question.questionTitle.length > 50 ) 
+    {
+        return { error: 'Question string should be between 3 and 30 characters' };
+    }
+
+    // Checks if the question has between 2 to 6 answers
+    if (question.answers.length < 2 || question.answers.length > 6)
+    {
+        return { error: 'The question should have between 2 to 6 answers' };
+    }
+
+    // Checks if question.duration is positive
+    if (question.duration < 0)
+    {
+        return { error: 'Question duration must be a positive number' };
+    }
+
+    // Checks if totalQuestionDuration exceeds 3 mintues
+    const totalQuestionDuration = data.questions.filter((question) => question.quizId === quizId).reduce((sum, currQues) => sum + currQues.duration, 0);
+    if ((totalQuestionDuration + question.duration) > 180)
+    {
+        return { error: 'The sum of the question durations in the quiz exceeds 3 minutes' };
+    }
+
+    // Checks if question points are valid
+    if (question.points < 1 || question.points > 10)
+    {
+        return { error: 'The points awarded for the question should be between 1 and 10' };
+    }
+
+    // Check if Answer Length is Invalid
+    const invalidAnswerTitle = question.answers.find((answer) => answer.answerTitle.length < 1 || answer.answerTitle.length > 30)
+    if (invalidAnswerTitle !== undefined)
+    {
+        return { error: 'Answer length should be between 1 and 30 characters' };
+    }
+
+    // Check if answer titles contain a duplicate
+    const answerTitles = question.answers.map(answer => answer.answerTitle);
+    if (answerTitles.some((title, index) => answerTitles.indexOf(title) !== index)) 
+    {
+        return { error: 'Answers should not contain duplicates' };
+    }
+
+    // Checks if there are no correct answers
+    const correctAnswer = question.answers.find((answer) => answer.correct === true)
+    if (correctAnswer === undefined)
+    {
+        return { error: 'There are no correct answers' };
+    }
+
+    const questionIdGenerator = data.questions.length + 1 
+    const tempQuestion = {
+        quizId: quizId,
+        questionId: questionIdGenerator,
+        questionTitle: question.questionTitle,
+        duration: question.duration,
+        points: question.points,
+        answers: question.answers
+    }
+    data.questions.push(tempQuestion);
+    setData(data);
+
+    return {
+        questionId: tempQuestion.questionId
+    }
+
+}
+
+
