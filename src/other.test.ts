ddcdf05bf9d16d67573a1685e7985e04f48cb7f7
Ansh Nimbalkar <z5415892@ad.unsw.edu.au>
2023-11-09 16:42:53 +1100

    Added clear request function

--- src/other.test.ts
+++ src/other.test.ts
@@ -1,7 +1,12 @@
-// import request from 'sync-request-curl';
-// import { port, url } from './config.json';
-// import { clear } from './other';
-// const SERVER_URL = `${url}:${port}`;
+import request from 'sync-request-curl';
+import { port, url } from './config.json';
+import { clear } from './other';
+const SERVER_URL = `${url}:${port}`;
+
+export function clearRequest() {
+  const res = request('DELETE', SERVER_URL + '/v1/clear');
+  return JSON.parse(res.body as string);
+}
 
 test('Nice Test', () => {
   expect(1 + 1).toEqual(2);
