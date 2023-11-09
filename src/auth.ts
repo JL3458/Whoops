24e25b6591d29f8a3f347e45e9f0bf9a3713b513
Ansh Nimbalkar <z5415892@ad.unsw.edu.au>
2023-11-09 16:44:31 +1100

    Updated implementaton for adminAuthRegister and adminAuthLogin

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,7 @@
-import { randomUUID } from 'crypto';
+import { randomUUID, createHash } from 'crypto';
 import { getData, setData, token } from './dataStore';
 import validator from 'validator';
+import HTTPError from 'http-errors';
 
 interface ErrorReturn {
     error: string
@@ -30,30 +31,30 @@ export function adminAuthRegister(email: string, password: string, nameFirst: st
 
   // Checking if email is valid
   if (validator.isEmail(email) === false) {
-    return { error: 'Invalid Email entered' };
+    throw HTTPError(400, 'Invalid Email Entered');
   }
 
   // Checking if email is in use by another user
   for (const user of data.users) {
     if (user.email === email) {
-      return { error: 'Entered Email is Already in Use' };
+      throw HTTPError(400, 'Entered Email is Already in Use');
     }
   }
   // Checking if first and last name meet the required conditions
   if (nameFirst.length < 2 || nameFirst.length > 20 || PATTERN.test(nameFirst) === false) {
-    return { error: 'Invalid First Name' };
+    throw HTTPError(400, 'Invalid First Name');
   }
   if (nameLast.length < 2 || nameLast.length > 20 || PATTERN.test(nameLast) === false) {
-    return { error: 'Invalid Last Name' };
+    throw HTTPError(400, 'Invalid Last Name');
   }
 
   // Checking if password meets the required conditions
   if (password.length < 8 || LETTERS.test(password) === false || NUMS.test(password) === false) {
-    return { error: 'Invalid Password' };
+    throw HTTPError(400, 'Invalid Password');
   }
 
   const Id = data.users.length + 1;
-  data.users.push({ userId: Id, email: email, password: password, nameFirst: nameFirst, nameLast: nameLast, numSuccessfulLogins: 1, numFailedPasswordsSinceLastLogin: 0, oldPasswords: [password] });
+  data.users.push({ userId: Id, email: email, password: hashPassword(password), nameFirst: nameFirst, nameLast: nameLast, numSuccessfulLogins: 1, numFailedPasswordsSinceLastLogin: 0, oldPasswords: [password] });
   const newToken = startSession(Id);
   const encodeToken = encodeURIComponent(JSON.stringify(newToken));
   setData(data);
@@ -69,13 +70,15 @@ export function adminAuthLogin(email: string, password: string): AuthReturn | Er
   // Checking if user with email exists
   const user = data.users.find((user) => user.email === email);
   if (user === undefined) {
-    return { error: 'Email does not exist' };
+    throw HTTPError(400, 'Email does not exist');
   }
 
+  const passwordHashed = hashPassword(password);
+
   // Checking whether password entered is incorrect
-  if (user.password !== password) {
+  if (user.password !== passwordHashed) {
     user.numFailedPasswordsSinceLastLogin = user.numFailedPasswordsSinceLastLogin + 1;
-    return { error: 'Incorrect Password' };
+    throw HTTPError(400, 'Incorrect Password');
   }
 
   // Updating details about user
@@ -214,6 +217,10 @@ export function adminUserPassword(token: string, oldPassword: string, newPasswor
 }
 
 /// /////////////////////////// Helper Functions ////////////////////////////////
+function hashPassword(password: string) {
+  const hash = createHash('sha256').update(password).digest('hex');
+  return hash;
+}
 
 function isEqual(currentToken: token, tempToken: token): boolean {
   return JSON.stringify(currentToken) === JSON.stringify(tempToken);
@@ -260,7 +267,11 @@ function startSession(authUserId: number): token {
     data.tokens.push(newToken);
     setData(data);
     return newToken;
+  } else {
+    data.tokens = data.tokens.filter(currentToken => !isEqual(currentToken, findToken));
+    const newToken = { userId: authUserId, sessionId: randomUUID() };
+    data.tokens.push(newToken);
+    setData(data);
+    return newToken;
   }
-
-  return findToken;
 }
