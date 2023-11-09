e4549926f347c2533922f8c75ba52fcc5a1075c2
Ansh Nimbalkar <z5415892@ad.unsw.edu.au>
2023-11-09 17:11:47 +1100

    Testing for adminAuthLogout

--- src/auth.test.ts
+++ src/auth.test.ts
@@ -8,6 +8,7 @@ import { IncomingHttpHeaders } from 'http';
 
 const SERVER_URL = `${url}:${port}`;
 const TIMEOUT_MS = 10000;
+const ERROR = { error: expect.any(String) };
 
 interface Payload {
   [key: string]: any;
@@ -75,7 +76,24 @@ export function authLoginRequest(email: string, password: string) {
   return requestHelper('POST', '/v1/admin/auth/login', { email, password }, {});
 }
 
-//////////////////////////////// Tests /////////////////////////////////
+export function authLogoutRequest(token: string) {
+  return requestHelper('POST', '/v2/admin/auth/logout', {}, { token });
+}
+
+export function userDetailsRequest(token: string) {
+  const res = request(
+    'GET',
+    SERVER_URL + '/v1/admin/user/details',
+    {
+      qs: {
+        token
+      }
+    }
+  );
+  return JSON.parse(res.body.toString());
+}
+
+/// ///////////////////////////// Tests /////////////////////////////////
 
 beforeEach(() => {
   clearRequest();
@@ -98,20 +116,19 @@ describe('Tests for adminAuthRegister', () => {
     expect(() => authRegisterRequest('InvalidEmail@gmail.com', 'password123', 'Pedro123', 'Gonzalez')).toThrow(HTTPError[400]);
     expect(() => authRegisterRequest('InvalidEmail@gmail.com', 'password123', 'P', 'Gonzalez')).toThrow(HTTPError[400]);
     expect(() => authRegisterRequest('InvalidEmail@gmail.com', 'password123', 'Pedrooooooooooooooooo', 'Gonzalez')).toThrow(HTTPError[400]);
-});
+  });
 
-test('Invalid last name', () => {
+  test('Invalid last name', () => {
     expect(() => authRegisterRequest('InvalidEmail@gmail.com', 'password123', 'Pedro', 'Gonzalez123')).toThrow(HTTPError[400]);
     expect(() => authRegisterRequest('InvalidEmail@gmail.com', 'password123', 'Pedro', 'G')).toThrow(HTTPError[400]);
     expect(() => authRegisterRequest('InvalidEmail@gmail.com', 'password123', 'Pedro', 'Gonzalezzzzzzzzzzzzzz')).toThrow(HTTPError[400]);
-});
+  });
 
-test('Invalid password', () => {
+  test('Invalid password', () => {
     expect(() => authRegisterRequest('InvalidEmail@gmail.com', 'pass123', 'Pedro', 'Gonzalez')).toThrow(HTTPError[400]);
     expect(() => authRegisterRequest('InvalidEmail@gmail.com', 'password', 'Pedro', 'Gonzalez')).toThrow(HTTPError[400]);
     expect(() => authRegisterRequest('InvalidEmail@gmail.com', '12345678', 'Pedro', 'Gonzalez')).toThrow(HTTPError[400]);
-});
-
+  });
 
   test('Valid Tests with same email error', () => {
     expect(authRegisterRequest('ValidEmail1@gmail.com', 'pass1234', 'Pedro', 'Gonzalez')).toEqual({ token: expect.any(String) });
@@ -168,3 +185,41 @@ describe('Tests for adminAuthlogin', () => {
   });
 });
 
+describe('Tests for adminAuthlogout', () => {
+  beforeEach(() => {
+    clearRequest();
+  });
+
+  test('Token is empty or invalid', () => {
+    expect(authRegisterRequest('ValidEmail1@gmail.com', 'password123', 'Pedro', 'Gonzalez')).toEqual({ token: expect.any(String) });
+    expect(() => authLogoutRequest('')).toThrow(HTTPError[401]);
+  });
+
+  test('Valid tests', () => {
+    const token1 = authRegisterRequest('ValidEmail1@gmail.com', 'password123', 'Pedro', 'Gonzalez');
+    expect(userDetailsRequest(token1.token)).toEqual({
+      user: {
+        userId: expect.any(Number),
+        name: 'Pedro Gonzalez',
+        email: 'ValidEmail1@gmail.com',
+        numSuccessfulLogins: 1,
+        numFailedPasswordsSinceLastLogin: 0
+      }
+    });
+    expect(authLogoutRequest(token1.token)).toEqual({});
+
+    // Returns error since user has been logged out
+    expect(userDetailsRequest(token1.token)).toEqual(ERROR);
+  });
+
+  test('Valid tests with clear', () => {
+    const token1 = authRegisterRequest('ValidEmail1@gmail.com', 'password123', 'Pedro', 'Gonzalez');
+    expect(authLogoutRequest(token1.token)).toEqual({});
+    const token2 = authLoginRequest('ValidEmail1@gmail.com', 'password123');
+    expect(token2).toEqual({ token: expect.any(String) });
+    expect(clearRequest()).toEqual({});
+
+    // Now, unable to logout as the respective users have been cleared
+    expect(() => authLogoutRequest(token2.token)).toThrow(HTTPError[401]);
+  });
+});
