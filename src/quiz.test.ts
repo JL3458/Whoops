a2670159493eb7c4a6bfa62dee9151f71e53c520
Divakar Dessai <z5411688@ad.unsw.edu.au>
2023-11-09 17:27:17 +1100

    Added Request Handler Function, along with adminQuizCreate Server Implementation and appropriate tests for HTTPErrors

--- src/quiz.test.ts
+++ src/quiz.test.ts
@@ -1,11 +1,177 @@
-// // New .ts file to implement tests for quiz.ts functions through the server
+// New .ts file to implement tests for quiz.ts functions through the server
 
-// import request from 'sync-request-curl';
-// import { port, url } from './config.json';
-// import { clearRequest } from './other.test';
-// import { authRegisterRequest, authLoginRequest, authLogoutRequest } from './auth.test';
-// const SERVER_URL = `${url}:${port}`;
-// const ERROR = { error: expect.any(String) };
+import request, { HttpVerb } from 'sync-request-curl';
+import { port, url } from './config.json';
+
+// ADD THESE LATER PLEASE
+import { clearRequest } from './other.test';
+import { authRegisterRequest, authLoginRequest, authLogoutRequest } from './auth.test';
+
+import { IncomingHttpHeaders } from 'http';
+import HTTPError from 'http-errors';
+
+const SERVER_URL = `${url}:${port}`;
+const TIMEOUT_MS = 10000;
+
+interface Payload {
+  [key: string]: any;
+}
+
+///////////////////////////////// Request Helper Function Wrapper ////////////////////////////////
+
+
+// Helpers
+const requestHelper = (
+  method: HttpVerb,
+  path: string,
+  payload: Payload,
+  headers: IncomingHttpHeaders = {}
+): any => {
+  let qs = {};
+  let json = {};
+  if (['GET', 'DELETE'].includes(method.toUpperCase())) {
+    qs = payload;
+  } else {
+    // PUT/POST
+    json = payload;
+  }
+
+  const url = SERVER_URL + path;
+  const res = request(method, url, { qs, json, headers, timeout: TIMEOUT_MS });
+
+  let responseBody: any;
+  try {
+    responseBody = JSON.parse(res.body.toString());
+  } catch (err: any) {
+    if (res.statusCode === 200) {
+      throw HTTPError(500,
+        `Non-jsonifiable body despite code 200: '${res.body}'.\nCheck that you are not doing res.json(undefined) instead of res.json({}), e.g. in '/clear'`
+      );
+    }
+    responseBody = { error: `Failed to parse JSON: '${err.message}'` };
+  }
+
+  const errorMessage = `[${res.statusCode}] ` + responseBody?.error || responseBody || 'No message specified!';
+
+  // NOTE: the error is rethrown in the test below. This is useful becasuse the
+  // test suite will halt (stop) if there's an error, rather than carry on and
+  // potentially failing on a different expect statement without useful outputs
+  switch (res.statusCode) {
+    case 400: // BAD_REQUEST
+    case 401: // UNAUTHORIZED
+      throw HTTPError(res.statusCode, errorMessage);
+    case 404: // NOT_FOUND
+      throw HTTPError(res.statusCode, `Cannot find '${url}' [${method}]\nReason: ${errorMessage}\n\nHint: Check that your server.ts have the correct path AND method`);
+    case 500: // INTERNAL_SERVER_ERROR
+      throw HTTPError(res.statusCode, errorMessage + '\n\nHint: Your server crashed. Check the server log!\n');
+    default:
+      if (res.statusCode !== 200) {
+        throw HTTPError(res.statusCode, errorMessage + `\n\nSorry, no idea! Look up the status code ${res.statusCode} online!\n`);
+      }
+  }
+  return responseBody;
+};
+
+////////////////////////// Server Functions ///////////////////////////
+
+
+export function adminQuizCreateRequest(token: string, name: string, description: string) {
+  return requestHelper('POST', '/v2/admin/quiz', { name, description }, { token })
+}
+
+
+///////////////////////////// Main Tests /////////////////////////////
+
+describe('Tests of adminQuizCreate', () => {
+  beforeEach(() => {
+    clearRequest();
+  });
+
+  test('Token is Empty', () => {
+    expect(() => adminQuizCreateRequest('', 'Test Quiz 1', 'This is a sample test')).toThrow(HTTPError[401]);
+  });
+
+  test('Normal Case correct output value', () => {
+    const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+    expect(adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'Sample Quiz Testing')).toEqual({ quizId: expect.any(Number) });
+    expect(adminQuizCreateRequest(newUser.token, '              ', '')).toEqual({ quizId: expect.any(Number) });
+    expect(adminQuizCreateRequest(newUser.token, 'Sam', 'The @3xpl0r3rs J0urn3y')).toEqual({ quizId: expect.any(Number) });
+    expect(adminQuizCreateRequest(newUser.token, 'Supercalifragilisticexpialidoc', 'Sample Quiz Testing')).toEqual({ quizId: expect.any(Number) });
+    expect(adminQuizCreateRequest(newUser.token, 'Test Quiz 2', 'Exploring the cosmos, seeking answers, and embracing the unknown.')).toEqual({ quizId: expect.any(Number) });
+  });
+
+  test('Invalid Quiz Names', () => {
+    const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+    expect(() => adminQuizCreateRequest(newUser.token, '@test12345', 'Sample Quiz Testing')).toThrow(HTTPError[400]);
+    expect(() => adminQuizCreateRequest(newUser.token, '', 'Sample Quiz Testing')).toThrow(HTTPError[400]);
+    expect(() => adminQuizCreateRequest(newUser.token, 'Alexander Bartholomew Fitzwilliam Throckmorton Montgomery III', 'Sample Quiz Testing')).toThrow(HTTPError[400]);
+    expect(() => adminQuizCreateRequest(newUser.token, '!!!@@@@##########$$$$$$$$$$$$$**********&&&&&&&&&&&&^^^^^^^^^^', 'Sample Quiz Testing')).toThrow(HTTPError[400]);
+    expect(() => adminQuizCreateRequest(newUser.token, '!!!', 'Sample Quiz Testing')).toThrow(HTTPError[400]);
+    expect(() => adminQuizCreateRequest(newUser.token, 'J@n3 Sm!th', 'Sample Quiz Testing')).toThrow(HTTPError[400]);
+    expect(() => adminQuizCreateRequest(newUser.token, '                                    ', 'Sample Quiz Testing')).toThrow(HTTPError[400]);
+  });
+
+  test('Invalid Description Names', () => {
+    const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+    expect(() => adminQuizCreateRequest(newUser.token, 'Divakar Quiz 1', 'This Quiz is on the magnificent cosmos, seeking answers, and discovering wonders. Fantastic!fdvsvsfbrgberbertrtb')).toThrow(HTTPError[400]);
+  });
+
+  test('Quiz name already exists', () => {
+    const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+    adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'Sample Quiz Testing');
+    expect(() => adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'Surely, this test doesnt exist anymore???')).toThrow(HTTPError[400]);
+  });
+
+  test('User makes two quizes', () => {
+    const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+    adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'Sample Quiz Testing');
+    expect(adminQuizCreateRequest(newUser.token, 'Test Quiz 2', 'Surely, this test works')).toEqual({ quizId: expect.any(Number) });
+  });
+
+  test('Testing with clearRequest', () => {
+    let newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+    expect(adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'Surely, this test works')).toEqual({ quizId: expect.any(Number) });
+    expect(clearRequest()).toEqual({});
+
+    // Still able to create quiz with same name and user since previous quiz is cleared
+    newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+    expect(adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'Surely, this test works')).toEqual({ quizId: expect.any(Number) });
+  });
+
+  test('Quiz name already exists, but a new user is entering it', () => {
+    const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+    adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'Sample Quiz Testing');
+    const newUser2 = authRegisterRequest('Validemail2@gmail.com', 'password123', 'Ansh', 'Nimbalkar');
+    expect(adminQuizCreateRequest(newUser2.token, 'Test Quiz 1', 'This Test should be working')).toEqual({ quizId: expect.any(Number) });
+  });
+
+  test('Making Multiple Quizzes with one user', () => {
+    const newUser = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+    expect(adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'Made by Divakar')).toEqual({ quizId: expect.any(Number)});
+    expect(adminQuizCreateRequest(newUser.token, 'Test Quiz 2', 'Made by Divakar')).toEqual({ quizId: expect.any(Number) });
+    expect(adminQuizCreateRequest(newUser.token, 'Test Quiz 3', 'Made by Divakar')).toEqual({ quizId: expect.any(Number) });
+  
+    // We get an expected error as the quizzes are already created
+    expect(() => adminQuizCreateRequest(newUser.token, 'Test Quiz 1', 'Made by Divakar')).toThrow(HTTPError[400]);
+    expect(() => adminQuizCreateRequest(newUser.token, 'Test Quiz 2', 'Made by Divakar')).toThrow(HTTPError[400]);
+    expect(() => adminQuizCreateRequest(newUser.token, 'Test Quiz 3', 'Made by Divakar')).toThrow(HTTPError[400]);
+  });
+  
+  test('Making Multiple Quizzes with Multiple Users', () => {
+    const newUser1 = authRegisterRequest('Validemail@gmail.com', 'password123', 'Divakar', 'Dessai');
+    const newUser2 = authRegisterRequest('Validemails@gmail.com', 'password123', 'Jason', 'Mascheranous');
+    const newUser3 = authRegisterRequest('Validemailt@gmail.com', 'password123', 'Sanath', 'Nevagi');
+    expect(adminQuizCreateRequest(newUser1.token, 'Test Quiz 1', 'Made by Divakar')).toEqual({ quizId: expect.any(Number) });
+    expect(adminQuizCreateRequest(newUser2.token, 'Test Quiz 2', 'Made by Jason')).toEqual({ quizId: expect.any(Number) });
+    expect(adminQuizCreateRequest(newUser3.token, 'Test Quiz 3', 'Made by Sanath')).toEqual({ quizId: expect.any(Number) });
+  
+    // We get an expected error as the quizzes are already created
+    expect(() => adminQuizCreateRequest(newUser1.token, 'Test Quiz 1', 'Made by Divakar')).toThrow(HTTPError[400]);
+    expect(() => adminQuizCreateRequest(newUser2.token, 'Test Quiz 2', 'Made by Jason')).toThrow(HTTPError[400]);
+    expect(() => adminQuizCreateRequest(newUser3.token, 'Test Quiz 3', 'Made by Sanath')).toThrow(HTTPError[400]);
+  });
+});
+
 
 test('Nice Test', () => {
   expect(1 + 1).toEqual(2);
