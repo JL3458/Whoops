840aa9f751668bbec97b44c7531cd5f681286427
Shervin <z5479717@ad.unsw.edu.au>
2023-11-10 17:58:37 +1100

    updating descriptionupdate

--- src/server.ts
+++ src/server.ts
@@ -216,19 +216,12 @@ app.put('/v2/admin/quiz/:quizid/name', (req: Request, res: Response) => {
 });
 
 // adminQuizDescriptionUpdate
-app.put('/v1/admin/quiz/:quizid/description', (req: Request, res: Response) => {
-  const { token, description } = req.body;
+app.put('/v2/admin/quiz/:quizid/description', (req: Request, res: Response) => {
+  const { description } = req.body;
+  const token = req.headers.token as string;
   const quizId = parseInt(req.params.quizid);
   const response = adminQuizDescriptionUpdate(token, quizId, description);
-  if (response.error) {
-    if (response.error === 'Token is empty or invalid') {
-      return res.status(401).json(response);
-    } else if (response.error === 'Valid token is provided, but user is not an owner of this quiz') {
-      return res.status(403).json(response);
-    } else if ('error' in response) {
-      return res.status(400).json(response);
-    }
-  }
+
   saveDataStore();
   res.json(response);
 });
